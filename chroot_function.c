86196ed2214d1236cc30a3856a9fb90f18a7d02d
Magniko <magniko1524@gmail.com>
2021-01-27 11:57:30 +0100

    first

--- chroot_function.c
+++ chroot_function.c
@@ -1,10 +1,16 @@
+/*Denne koden setter cwd(current word directory) som rot for prosessen slik at man ikke har tillgang til filer ovenfor cwd.
+For å bruke denne koden til nettleseren kopierer man bare den inn i koden
+til prosessen man ønsker å endre roten til cwd og kalle chroot_function() før resten av koden kjører
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 static void chroot_function();
 
-
+//main function
 int main(int argc, char* argv[]) {
 	chroot_function();
 
@@ -12,17 +18,24 @@ int main(int argc, char* argv[]) {
 
 static void chroot_function()
 {
-
+	//this only gets the cwd the process is running in
+	//this code can be commented/removed in real applications
+	///*
     char buf[1024];
     getcwd(buf, 1024);
     printf("current working directory: %s\n", buf);
+    //*/
 
+    //calling the chroot process and sets "." as the root directory
+    //if error code -1 is returned, the code exits
     if(chroot(".") == -1) {
         perror("chroot");
         exit(EXIT_FAILURE);
     }
+    ///*
     getcwd(buf, 1024);
     printf("current working directory: %s\n", buf);
+    //*/
 
 
 }
--- daemonizer.c
+++ daemonizer.c
@@ -1,3 +1,8 @@
+/*Denne koden oppretter en demonisert prosess som foregår i bakgrunnen.
+For å bruke denne koden til nettleseren kopierer man bare den inn i koden
+til prosessen man ønsker å demonisere og kalle daemonizer() før resten av koden kjører
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,7 +10,7 @@
 
 static void daemonizer();
 
-
+//main function
 int main(int argc, char* argv[]) {
     daemonizer();
 
@@ -13,21 +18,32 @@ int main(int argc, char* argv[]) {
 
 static void daemonizer()
 {
+    //create a process ID variable
     pid_t pid;
 
+    //creates a fork of the process, a parent and a child
+    //the code should run identical in practice for both child and parent
+    //but they each have a different value assigned to the pid variable
+    //a child has pid = 0 and the parent pid > 0. a pid < 0 means the forking failed
+    //this is so that the process can run in the background and so that it is not a process group leader
     pid = fork();
+
 
     if (pid < 0)
         exit(EXIT_FAILURE);
-
+    //exits the parent process. Now only the child is running
     if (pid > 0)
         exit(EXIT_SUCCESS);
 
+    //creates a session from the child process and assigns it the lead session ID
+    //if this process fails(returns > 0) the child process terminates 
     if (setsid() < 0)
         exit(EXIT_FAILURE);
 
+    //this is to ignore the SIGHUP signal which is sent whenever a session leader process is terminated
     signal(SIGHUP, SIG_IGN);
 
+    //forks the process again to ensure that it's no longer a session leader and cannot be accessed through an available terminal
     pid = fork();
 
     if (pid < 0)
@@ -36,7 +52,9 @@ static void daemonizer()
     if (pid > 0)
         exit(EXIT_SUCCESS);
 
-
+    //this code here is just to test if the daemonizer works.
+    //by doing cat log.txt, you should see if it writes "Test" to the file every 1 sec
+    ///*
     FILE *f = fopen("log.txt", "w");
     if(f == NULL)
         exit(EXIT_FAILURE);
@@ -48,4 +66,5 @@ static void daemonizer()
         fflush(f);
     }
     fclose(f);
+    //*/
 }
